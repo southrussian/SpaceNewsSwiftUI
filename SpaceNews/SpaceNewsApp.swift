65f8613f39c8ba86eac71f2c43005cf909edf76a
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-06-23 23:56:47 +0300

    Initial Commit

---
+++ SpaceNews/SpaceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsApp.swift
+//  SpaceNews
+//
+//  Created by Danil Peregorodiev on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
