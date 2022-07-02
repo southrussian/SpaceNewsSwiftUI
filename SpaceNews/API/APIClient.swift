74387f94463bb5df61e5672c6f8ca680eab62faa
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-07-02 14:10:18 +0300

    Added SpaceAPI class

--- SpaceNews/API/APIClient.swift
+++ SpaceNews/API/APIClient.swift
@@ -5,6 +5,8 @@
 //  Created by Danil Peregorodiev on 29.06.2022.
 //
 
+// https://api.spaceflightnewsapi.net/v3/articles
+
 import Foundation
 
 struct SpaceData: Codable, Identifiable {
@@ -15,4 +17,26 @@ struct SpaceData: Codable, Identifiable {
     var summary: String
     var publishedAt: String
     var newsSite: String
+}
+
+@MainActor class SpaceAPI : ObservableObject {
+    @Published var news: [SpaceData] = []
+    
+    func getData() {
+        guard let url = URL(string: "https://api.spaceflightnewsapi.net/v3/articles")
+        else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                let tempError = error!.localizedDescription
+                DispatchQueue.main.async {
+                    self.news = [SpaceData(id: 0, title: tempError, url: "Error", imageUrl: "Error", summary: "Try to refresh when your Interner connection will be available", publishedAt: "Error", newsSite: "Error")]
+                }
+                return
+            }
+
+        }
+    }
 }
